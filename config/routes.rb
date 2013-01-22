a33d343184695f4fe8f20848ae937af2c83a9952
nickborromeo <ngborromeo@gmail.com>
2013-01-22 11:18:08 -0800

    add named routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Spreadsheetmerger::Application.routes.draw do
   root :to => 'spreadsheet#index'
   
   get 'signup', to: 'users#new', as: 'signup'
-  #get 'login', to: 'sessions#new', as: 'login'
-  #get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   
   resources :users
   resources :sessions
