25665d902691489ce4253909ba9c99864740f878
nickborromeo <ngborromeo@gmail.com>
2013-01-23 16:19:37 -0800

    Finish password reset feature.Add integration tests for password reset

--- spec/features/password_reset_spec.rb
+++ spec/features/password_reset_spec.rb
@@ -27,4 +27,38 @@ describe "Password Reset" do
     end
   end
   
+  context "when resetting password" do
+    
+    it "password is reset when confirmation is matched" do
+      user = FactoryGirl.create(:user, :password_reset_token => "secrettoken", :password_reset_sent_at => 1.hour.ago)
+      visit edit_password_reset_path(user.password_reset_token)
+      fill_in "user_password", with: "newpassword"
+      fill_in "user_password_confirmation", with: "newpassword"
+      click_button "Update Password"
+      page.should have_content("Password Updated Successfully")
+    end
+    
+    it "password not updated if confirmation does not match" do
+      user = FactoryGirl.create(:user, password_reset_token: "secrettoken", password_reset_sent_at: 1.hour.ago)
+      visit edit_password_reset_path(user.password_reset_token)
+      fill_in "user_password", with: "newpassword"
+      fill_in "user_password_confirmation", with: "wrongpassword"
+      click_button "Update Password"
+      page.should have_content("Password doesn't match confirmation")
+    end
+    
+    it "reports when token is expired" do
+      user = FactoryGirl.create(:user, password_reset_token: "secrettoken", password_reset_sent_at: 5.hours.ago)
+      visit edit_password_reset_path(user.password_reset_token)
+      fill_in "user_password", with: "newpassword"
+      fill_in "user_password_confirmation", with: "newpassword"
+      click_button "Update Password"
+      page.should have_content("Password reset token expired")
+    end
+    
+    it "raises record not found when token is invalid" do
+      expect { visit edit_password_reset_path("invalid_token") }.to raise_error
+    end
+  end
+  
 end
