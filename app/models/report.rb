349513080160dfd6bd408e3b026d23dc6426e436
Nick Borromeo <ngborromeo@gmail.com>
2013-02-11 17:53:39 -0800

    Unsuccessful attempt to write tests for Report Model. Create Integration tests first

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,12 @@
 class Report < ActiveRecord::Base
+
+  belongs_to :user
+
+  def self.open_report(file)
+    case File.extname(file)
+    when ".csv" then Csv.new(file)
+    when ".xls" then Excel.new(file)
+    else raise "Unknown file type: "#{file}"
+    end
+  end
 end
--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe Report do
   describe ".open_report" do
+    let(:file_path) { Rails.root.join('spec','support','files') }
     context "with valid file extension" do
-      it "can open CSV file"
+      it "can open CSV file" do
+        # file = file_path+"Report1.csv"
+        # Report.open_report(file).should be_a(Csv)
+      end
+      it "can open XLS file" do
+        # file = file_path + "Report1.xls"
+        # Report.open_report(file).should be_an(Excel)
+      end
       it "can open XLS file"
-      it "can open XLSX file"
     end
     
     context "with invalid file extension" do
