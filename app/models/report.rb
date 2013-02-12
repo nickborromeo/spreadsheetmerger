359549c6fed0a4e4b907762dbffd20dcb97b5823
Nick Borromeo <ngborromeo@gmail.com>
2013-02-12 14:30:03 -0800

    Add tests for uploading a single report

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,11 +2,16 @@ class Report < ActiveRecord::Base
 
   belongs_to :user
 
+  def self.import(file)
+    report = open_report(file)
+  end
+
   def self.open_report(file)
-    case File.extname(file)
-    when ".csv" then Csv.new(file)
-    when ".xls" then Excel.new(file)
-    else raise "Unknown file type: "#{file}"
+    case File.extname(file.original_filename)
+    when ".csv" then Csv.new(file.path, nil, :ignore)
+    when ".xls" then Excel.new(file.path, nil, :ignore)
+    when ".xlsx" then Excelx.new(file.path, nil, :ignore)
+    else raise "Unknown file type: "#{file.original_filename}"
     end
   end
 end
--- spec/features/report_upload_spec.rb
+++ spec/features/report_upload_spec.rb
@@ -2,6 +2,42 @@ require 'spec_helper'
 
 describe "Report Upload" do
   let(:user) { FactoryGirl.create(:user) }
+  let(:report_file_path) { Rails.root.join('spec','support','files') }
 
   #TODO: create tests for file upload
+  context "with invalid file types" do 
+    it "does not accept blank file names" do 
+      sign_in_with(user.email, user.password)
+      visit users_path
+      attach_file(:first_report, report_file_path + "Report1.doc" )
+      click_button "Import"
+      page.should have_content("Unknown File Type")
+    end
+  end
+
+  context "with valid file types" do 
+    it "can upload csv files" do
+      sign_in_with(user.email, user.password)
+      visit users_path
+      attach_file(:first_report, report_file_path + "Report1.csv")
+      click_button "Import"
+      page.should have_content("File Uploaded")
+    end
+
+    it "can upload xls files" do
+      sign_in_with(user.email, user.password)
+      visit users_path
+      attach_file(:first_report, report_file_path + "Report1.xls")
+      click_button "Import"
+      page.should have_content("File Uploaded")
+    end
+
+    it "can upload xlsx files" do
+      sign_in_with(user.email, user.password)
+      visit users_path
+      attach_file(:first_report, report_file_path + "Report1.xlsx")
+      click_button "Import"
+      page.should have_content("File Uploaded")
+    end
+  end
 end
